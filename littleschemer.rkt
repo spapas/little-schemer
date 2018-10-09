3a9365769888965e17834e7b92e7989f10aa6942
Serafeim Papastefanos <spapas@gmail.com>
2018-10-09 14:26:27 +0300

    Start adding star functions

--- littleschemer.rkt
+++ littleschemer.rkt
@@ -214,3 +214,15 @@
       (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
 
 ; CHAPTER 5
+(define rember*
+  (lambda (a l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l))
+     (cond
+       ((eq? (car l) a) (rember* a (cdr l)))
+       (else (cons (car l) (rember* a (cdr l))))))
+    (else
+     (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+     
