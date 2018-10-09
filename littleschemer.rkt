fd198c329df2acc792f260de3d3fadbe19223ddd
Serafeim Papastefanos <spapas@gmail.com>
2018-10-09 14:19:24 +0300

    Finish ch4

--- littleschemer.rkt
+++ littleschemer.rkt
@@ -157,9 +157,60 @@
       ((o< n m) 0)
       (else (add1 (o/ (o- n m) m))))))
 
-
 (define olength
   (lambda (lat)
     (cond
       ((null? lat) 0)
       (else (add1 (olength (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((eq? n 1) (car lat) )
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((eq? n 1) (cdr lat))     
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat)) ))))
+
+(define eqan?
+  (lambda (a1 a2)
+  (cond
+    ((and (number? a1) (number? a2)) (= a1 a2))
+    ((or (number? a1) (number? a2)) #f)
+    (else (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      ((eq? (car lat) a) (add1 (occur a (cdr lat))))
+      (else (occur a (cdr lat))))))
+
+(define one?
+  (lambda (n)
+    (= n 1)))
+
+(define rempick2
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))     
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+; CHAPTER 5
