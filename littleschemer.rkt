18223cd48f6e5ba871ec96fb5c47bfe8ab44d8c9
Serafeim Papastefanos <spapas@gmail.com>
2018-10-09 13:49:43 +0300

    Initial

---
+++ littleschemer.rkt
@@ -0,0 +1,165 @@
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
+
+; CHAPTER 2
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+
+; CHAPTER 3 
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a ) (cdr lat))
+      (else (cons (car lat) (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons old (cons new (cdr lat ))))
+      (else (cons (car lat) (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons new (cons old (cdr lat ))))
+      (else (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons new (cdr lat )))
+      (else (cons (car lat) (subst new old (cdr lat)))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a ) (multirember a (cdr lat)))
+      (else (cons (car lat) (multirember a (cdr lat)))))))    
+
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons old (cons new (multiinsertR new old (cdr lat )))))
+      (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons new (cons old (multiinsertL new old (cdr lat )))))
+      (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old ) (cons new (multisubst new old (cdr lat ))))
+      (else (cons (car lat) (multisubst new old (cdr lat)))))))
+
+; CHAPTER 4
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define o-
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define ox
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (ox n (sub1 m)))))))
+
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else
+       (cons (o+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o> (sub1 n) (sub1 m))))))
+
+(define o<
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o< (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (cond
+      ((o< n m) #f)
+      ((o> n m) #f)
+      (else #t))))
+
+(define o^
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (ox n (o^ n (sub1 m)))))))
+
+(define o/
+  (lambda (n m)
+    (cond
+      ((o< n m) 0)
+      (else (add1 (o/ (o- n m) m))))))
+
+
+(define olength
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (olength (cdr lat)))))))
